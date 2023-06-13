210bb464944279f6d8b66e4c6557a6d679bee075
Savlatbek009 <savlatbek009@gmail.com>
2023-06-14 02:01:14 +0500

    Code of page

--- js/main.js
+++ js/main.js
@@ -102,3 +102,22 @@ dark_mode_btn.addEventListener('click', function () {
     document.body.classList.add('dark-mode')
     closeBtn.classList.add('btn-white')
 })
+
+// owl
+
+$(".home-owl1").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+        0: {
+            items: 1,
+        },
+        600: {
+            items: 2,
+        },
+        1000: {
+            items: 3,
+        },
+    },
+});
